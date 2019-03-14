1377eef69f76b5fe3c6acffb4bf4d799817abaf3
yunwuxin <448901948@qq.com>
2019-03-14 17:43:53 +0800

    提交

---
+++ src/Factory.php
@@ -0,0 +1,105 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2019 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: yunwuxin <448901948@qq.com>
+// +----------------------------------------------------------------------
+declare (strict_types = 1);
+
+namespace think;
+
+
+use InvalidArgumentException;
+use think\helper\Str;
+
+abstract class Factory
+{
+    protected $app;
+
+    /**
+     * 驱动
+     * @var array
+     */
+    protected $drivers = [];
+
+    /**
+     * 驱动的命名空间
+     * @var string
+     */
+    protected $namespace = "";
+
+    public function __construct(App $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * 获取驱动实例
+     * @param null $driver
+     * @return mixed
+     */
+    public function driver($driver = null)
+    {
+        $driver = $driver ?: $this->getDefaultDriver();
+
+        if (is_null($driver)) {
+            throw new InvalidArgumentException(sprintf(
+                'Unable to resolve NULL driver for [%s].', static::class
+            ));
+        }
+
+        if (!isset($this->drivers[$driver])) {
+            $this->drivers[$driver] = $this->createDriver($driver);
+        }
+
+        return $this->drivers[$driver];
+    }
+
+    /**
+     * 创建驱动
+     *
+     * @param  string $driver
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function createDriver($driver)
+    {
+        $method = 'create' . Str::studly($driver) . 'Driver';
+
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
+
+        if ($this->namespace || false !== strpos($driver, '\\')) {
+            $class = false !== strpos($driver, '\\') ? $driver : $this->namespace . Str::studly($driver);
+
+            if (class_exists($class)) {
+                return $this->app->make($class);
+            }
+        }
+
+        throw new InvalidArgumentException("Driver [$driver] not supported.");
+    }
+
+    /**
+     * 默认驱动
+     * @return string
+     */
+    abstract public function getDefaultDriver();
+
+    /**
+     * 动态调用
+     * @param string $method
+     * @param array  $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        return $this->driver()->$method(...$parameters);
+    }
+}
